d8db97d22f8277f76f189c518a20c505f9ffd21f
Lucas Braga <lucasbbs@live.fr>
2025-08-25 22:59:57 -0400

    Create official-solution.js

---
+++ two-pointers/find-nonduplicate-number-instances/official-solution.js
@@ -0,0 +1,14 @@
+class Solution {
+  moveElements(arr) {
+    let nextNonDuplicate = 1;
+    let i = 0;
+    while (i < arr.length) {
+      if (arr[nextNonDuplicate - 1] !== arr[i]) {
+        arr[nextNonDuplicate] = arr[i];
+        nextNonDuplicate += 1;
+      }
+      i += 1;
+    }
+    return nextNonDuplicate;
+  }
+}
